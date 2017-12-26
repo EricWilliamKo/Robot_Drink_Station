9cc431cc3389c1995127e2062671ea5542de90e9
Eric <blueeyesnake@gmail.com>
2017-12-26 12:17:14 +0000

    arm module motion tested

--- test/arm_serial_test.py
+++ test/arm_serial_test.py
@@ -2,11 +2,12 @@
 import serial
 import time
 import threading
+from threading import Thread
 
 armSerial = serial.Serial("/dev/arm",115200)
 
 def moveArm():
-    cmd = '#1P1800T1000\r\n#1P1500T1000\r\n'
+    cmd = '#1P1500T1000\r\n#1P1500T1000\r\n'
     armSerial.write(cmd)
     print cmd
 
@@ -19,21 +20,40 @@ def move2():
     cmd = '#1P1800T1000\r\n'
     cmd2 = '#1P1500T1000\r\n'
     armSerial.write(cmd+end)
+    print cmd
     time.sleep(1)
     armSerial.write(cmd2+end)
+    print cmd2
     time.sleep(1)
     print 'done'
+
+class Move(Thread):
+    def run(self):
+        end = '\r\n'
+        cmd = '#1P1800T1000\r\n'
+        cmd2 = '#1P1500T1000\r\n'
+        armSerial.write(cmd+end)
+        print cmd
+        time.sleep(1)
+        armSerial.write(cmd2+end)
+        print cmd2
+        time.sleep(1)
+        print 'done'
+
 
 
 
 if __name__ == '__main__':
     time.sleep(1)
-
-    moveArm()
+    # Move().start()
+    # move2().start()
+    # moveArm()
 
     serialThread = threading.Thread(target = move2)
     serialThread.daemon = True
     serialThread.start()
+
+    time.sleep(5)
 
     
     
